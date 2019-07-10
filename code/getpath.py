35e709b2621290a314ef0129e11dec8ea6b0db4c
tianxigao2 <tianxigao2@gmail.com>
2019-07-10 14:45:53 -0400

    rm alot

--- code/getpath.py
+++
@@ -1,6 +0,0 @@
-# THE PATH IS USED FOR LOCAL DATASET FILES
-# TO USE BRIDGES, USE THE getpath FUNTION STORED IN REMOTE COMPUTER
-
-def getpath():
-    return '/home/user1/REUS/image-reconstruction-2019/data/'
-
